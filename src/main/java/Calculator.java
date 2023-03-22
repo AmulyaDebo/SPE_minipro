1ec4e9f9ad7b5050e4a51a77c8aeab80ffd74719
AmulyaDebo <90204024+AmulyaDebo@users.noreply.github.com>
2023-03-22 12:03:29 +0530

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Calculator {
-
+// cqlculator
     private static final Logger logger = LogManager.getLogger(Calculator.class);
     public Calculator() {
     }
